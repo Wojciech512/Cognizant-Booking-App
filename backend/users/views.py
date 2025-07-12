08f65410fef601b1b971c421e53623d43b092429
Wojtek Wyleżoł <w.wylezol@wp.pl>
2025-07-13 00:30:14 +0200

    users register and login

---
+++ backend/users/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import UserRegisterSerializer
+
+
+class RegisterUserView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+
+
+class AppTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["is_staff"] = user.is_staff
+        token["username"] = user.username
+        return token
+
+
+class AppTokenObtainPairView(TokenObtainPairView):
+    serializer_class = AppTokenObtainPairSerializer
