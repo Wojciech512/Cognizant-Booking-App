0e14e392333dd08fa6b7acd8a0ce1bf0e01723b2
Wojtek Wyleżoł <w.wylezol@wp.pl>
2025-07-13 21:10:43 +0200

    frontend - logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { logout } from './features/auth/state/auth.actions';
+import { selectIsAuthenticated } from './features/auth/state/auth.selectors';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
 })
-export class AppComponent {}
+export class AppComponent {
+  isAuthenticated$: Observable<boolean>;
+
+  constructor(private store: Store) {
+    this.isAuthenticated$ = this.store.select(selectIsAuthenticated);
+  }
+
+  logout() {
+    this.store.dispatch(logout());
+  }
+}
