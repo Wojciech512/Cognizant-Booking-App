9389af4235297d69d609be3c4c31eae0aae57376
Wojtek Wyleżoł <w.wylezol@wp.pl>
2025-07-15 18:57:52 +0200

    admin and calendar folder structure fix

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/components/login/login.component';
 import { RegisterComponent } from './features/auth/components/register/register.component';
 import { AuthGuard } from './features/auth/guards/auth.guard';
-import { AdminComponent } from './features/admin/admin.component';
-import { CalendarComponent } from './features/calendar/calendar.component';
+import { AdminComponent } from './features/admin/components/admin.component';
+import { CalendarComponent } from './features/calendar/components/calendar.component';
 import { NoAuthGuard } from './features/auth/guards/no-auth.guard';
 import { IsStaffGuard} from './features/auth/guards/is-staff.guard';
 
