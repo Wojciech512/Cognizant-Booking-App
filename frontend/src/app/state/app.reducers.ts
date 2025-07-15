0e32f96d436153b2e0d25f0df0ea37e461dc5ced
Wojtek Wyleżoł <w.wylezol@wp.pl>
2025-07-15 22:07:54 +0200

    reducers registration

--- frontend/src/app/state/app.reducers.ts
+++ frontend/src/app/state/app.reducers.ts
@@ -1,7 +1,13 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { AppState } from './app.state';
 import { authReducer } from '../features/auth/state/auth.reducer';
+import { eventCategoryReducer } from '../features/calendar/state/event-category.reducer';
+import { timeSlotReducer } from '../features/calendar/state/time-slot.reducer';
+import { bookingReducer } from '../features/calendar/state/booking.reducer';
 
 export const appReducers: ActionReducerMap<AppState> = {
   auth: authReducer,
+  eventCategories: eventCategoryReducer,
+  timeSlots: timeSlotReducer,
+  bookings: bookingReducer,
 };
--- frontend/src/app/state/app.state.ts
+++ frontend/src/app/state/app.state.ts
@@ -1,5 +1,11 @@
 import { AuthState } from '../features/auth/models/auth.models';
+import { BookingState } from '../features/calendar/state/booking.reducer';
+import { TimeSlotState } from '../features/calendar/state/time-slot.reducer';
+import { EventCategoryState } from '../features/calendar/models/event-category.model';
 
 export interface AppState {
   auth: AuthState;
+  eventCategories: EventCategoryState;
+  timeSlots: TimeSlotState;
+  bookings: BookingState;
 }
